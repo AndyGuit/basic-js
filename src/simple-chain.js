f2ad149dd285f3f6888a44e5db09c1799c9266aa
AndyGuit <totalslash@msn.com>
2023-12-15 12:03:49 +0100

    feat: add sort by height solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -14,7 +14,7 @@ const chainMaker = {
     return this;
   },
   removeLink(position) {
-    if (typeof position !== 'number' || !Number.isInteger(position) || position <= 0 || position > this.chain.length) {
+    if (typeof position !== 'number' || position <= 0 || position > this.chain.length) {
       this.chain = [];
       throw Error('You can\'t remove incorrect link!');
     }
